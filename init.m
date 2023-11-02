004caaf74db66ee0d35055fb1aad10a1595d914d
dani-mg-05 <danimg0502@gmail.com>
2023-11-02 12:18:21 +0100

    Fichero export and import modified map

--- init.m
+++ init.m
@@ -2,4 +2,8 @@ close all;
 clear all;
 
 %% INICIALIZACIÓN DE ROS
-rosinit('http://172.22.32.153:11311' , 'NodeHost' , '172.22.24.95');
+%rosinit('http://172.22.32.153:11311' , 'NodeHost' , '172.22.24.95');
+
+%rosinit('http://172.29.30.177:11311' , 'NodeHost' , '172.29.29.55');
+
+rosinit('http://192.168.1.137:11311', 'NodeHost', '192.168.1.141');
